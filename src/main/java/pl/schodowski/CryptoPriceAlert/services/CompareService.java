5c36be88ac046f9be340e420d93797cb710b45fc
Piotr Schodowski <ps.schodowski@gmail.com>
2023-12-15 10:36:51 +0100

    MongoConfig WebFluxConfig

--- src/main/java/pl/schodowski/CryptoPriceAlert/services/CompareService.java
+++ src/main/java/pl/schodowski/CryptoPriceAlert/services/CompareService.java
@@ -11,18 +11,23 @@ public class CompareService {
 
     private final CryptoRepo cryptoRepo;
 
-    public void compareCrypto(Crypto cryptoAfterScrapping){
-
-//        cryptoRepo.findByName(cryptoAfterScrapping.getName())
-//                .subscribe(cryptoBeforeScrapping -> {
-//                    if (cryptoBeforeScrapping.getPrice() < cryptoAfterScrapping.getPrice()) {
-//                        System.out.println("Price of " + cryptoBeforeScrapping.getName() + " has increased!");
-//                    } else if (cryptoBeforeScrapping.getPrice() > cryptoAfterScrapping.getPrice()) {
-//                        System.out.println("Price of " + cryptoBeforeScrapping.getName() + " has decreased!");
-//                    } else {
-//                        System.out.println("Price of " + cryptoBeforeScrapping.getName() + " has not changed!");
-//                    }
-//                });
+    public void compareManager(Crypto cryptoAfterScrapping){
 
     }
+
+//    public Crypto returnCryptoFromDatabase(Crypto cryptoAfterScrapping){
+//        cryptoRepo.findByName(cryptoAfterScrapping.getName()).
+//    }
+
+    public void updateCryptoToDatabase(Crypto cryptoAfterScrapping){
+
+    }
+
+    public void compareCrypto(Crypto cryptoAfterScrapping, Crypto cryptoFromDatabase){
+
+    }
+
+
+
+
 }

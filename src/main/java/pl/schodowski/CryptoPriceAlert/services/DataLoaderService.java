7c78dcc132ce074eb2aa81627f822dae5cbee2bd
Piotr Schodowski <ps.schodowski@gmail.com>
2024-01-03 11:28:06 +0100

    DataLoader reactive version done

--- src/main/java/pl/schodowski/CryptoPriceAlert/services/DataLoaderService.java
+++ src/main/java/pl/schodowski/CryptoPriceAlert/services/DataLoaderService.java
@@ -6,7 +6,7 @@ import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Service;
 import pl.schodowski.CryptoPriceAlert.repo.Crypto;
 import pl.schodowski.CryptoPriceAlert.repo.CryptoRepo;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,11 +30,30 @@ public class DataLoaderService implements ApplicationListener<ContextRefreshedEv
     }
 
     void saveSelectedCryptoToDatabase(List<String> symbolsFromProperties) {
-        Flux<Crypto> cryptoFlux = Flux.fromIterable(apiService.getCryptoListFromCoinGecko())
-                .filter(crypto -> symbolsFromProperties.contains(crypto.getSymbol().toUpperCase()));
 
-        cryptoRepo.saveAll(cryptoFlux).subscribe();
+        List<Crypto> cryptoList = apiService.getCryptoListFromCoinGecko();
+
+        for (String symbol : symbolsFromProperties) {
+
+            cryptoList.stream()
+                    .filter(crypto -> symbol.equalsIgnoreCase(crypto.getSymbol()))
+                    .findFirst()
+                    .ifPresent(firstCrypto -> {
+                        Mono<Crypto> cryptoMono = Mono.just(firstCrypto);
+                        cryptoMono.subscribe(crypto -> {
+                            System.out.println("Symbol: " + crypto.getSymbol());
+                            System.out.println("Price: " + crypto.getPrice());
+                            System.out.println("Market Cap: " + crypto.getMarketCap());
+                            System.out.println("Total Volume: " + crypto.getTotalVolume());
+
+                            cryptoRepo.save(crypto).subscribe(savedCrypto -> {
+                                System.out.println("Crypto saved: " + savedCrypto);
+                            });
+                        });
+                    });
+        }
     }
+
 
     private List<String> makeListOfCryptoFromApplicationProperties() {
         return Arrays.asList(cryptoList.split(","));

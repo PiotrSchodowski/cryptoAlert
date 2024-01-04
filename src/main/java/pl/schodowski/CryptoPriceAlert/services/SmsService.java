eb82c771e420d48692e7ed89d228bd8acb2080ac
Piotr Schodowski <ps.schodowski@gmail.com>
2024-01-04 13:02:58 +0100

    done

--- src/main/java/pl/schodowski/CryptoPriceAlert/services/SmsService.java
+++ src/main/java/pl/schodowski/CryptoPriceAlert/services/SmsService.java
@@ -2,6 +2,7 @@ package pl.schodowski.CryptoPriceAlert.services;
 
 import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import pl.schodowski.CryptoPriceAlert.exceptions.CryptoPriceAlertException;
@@ -10,12 +11,19 @@ import pl.schodowski.CryptoPriceAlert.exceptions.CryptoPriceAlertException;
 @Service
 public class SmsService {
 
-    private final String ACCOUNT_SID = "ACf690f10d89ab0ca7d80da14e696f153c";
-    private final String AUTH_TOKEN = "b43ef950564e1f3b25bc3f4bae6c008a";
-    private final String TWILIO_PHONE_NUMBER = "+12058585764";
-    private final String RECEIVER_PHONE_NUMBER = "+48798226220";
+    private final String ACCOUNT_SID;
+    private final String AUTH_TOKEN;
+    private final String TWILIO_PHONE_NUMBER;
+    private final String RECEIVER_PHONE_NUMBER;
 
-    public SmsService() {
+    public SmsService(@Value("${twilio.account_sid}") String ACCOUNT_SID,
+                      @Value("${twilio.auth_token}") String AUTH_TOKEN,
+                      @Value("${twilio.phone_number}") String TWILIO_PHONE_NUMBER,
+                      @Value("${twilio.receiver_phone_number}") String RECEIVER_PHONE_NUMBER) {
+        this.ACCOUNT_SID = ACCOUNT_SID;
+        this.AUTH_TOKEN = AUTH_TOKEN;
+        this.TWILIO_PHONE_NUMBER = TWILIO_PHONE_NUMBER;
+        this.RECEIVER_PHONE_NUMBER = RECEIVER_PHONE_NUMBER;
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
     }
 
